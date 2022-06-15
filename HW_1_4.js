ba90fa86d2aba7c1ad05071a49b91dd6506dbae0
Artur <69423332+balsis@users.noreply.github.com>
2022-06-15 22:09:44 +0300

    add alert

--- HW_1_4.js
+++ HW_1_4.js
@@ -9,20 +9,22 @@ const checkAge = function(age_1) {
     let age_3 = 60;
     
     if (isNaN(age_1)){
-        if (age_1==2 && typeof(age_1)==String){
-            console.log(`You don’t have access because your age is ${age_1}. It’s less then`);
-        }
     console.log('Warning! Enter a numeric value!');
+    alert ('Warning! Enter a numeric value!');
     }
 else if (age_1 < age_2){
     console.log(`You don’t have access because your age is ${age_1}. It’s less then`);
+    alert(`You don’t have access because your age is ${age_1}. It’s less then`);
 }
 else if (age_1 >= age_2 && age_1 < age_3) {
     console.log('Welcome!');
+    alert ('Welcome!');
 }else if (age_1 > age_3){
     console.log('Keep calm and look Culture channel');
+    alert('Keep calm and look Culture channel');
 }else{
     console.log('Technical work');
+    alert('Technical work');
 }
     }
 
